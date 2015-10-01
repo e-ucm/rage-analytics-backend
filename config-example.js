d34411ba6b131f45492bc46ae28351591c18e379
RotaruDan <drotaru@ucm.es>
2015-10-01 13:42:24 +0200

    Adds 'nimbus.host' option when submitting a storm topology.

--- config-example.js
+++ config-example.js
@@ -2,25 +2,26 @@
 
 exports.port = process.env.PORT || '{{port}}';
 exports.mongodb = {
-  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || '{{mongodbUrl}}'
+    uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || '{{mongodbUrl}}'
 };
 exports.apiPath = '{{apiPath}}';
 exports.companyName = '{{companyName}}';
 exports.projectName = '{{projectName}}';
 exports.lrs = {
-  uri: process.env.LRS_URI || process.env.LRS_URL || '{{lrsUrl}}',
-  username: '{{lrsUsername}}',
-  password: '{{lrsPassword}}'
+    uri: process.env.LRS_URI || process.env.LRS_URL || '{{lrsUrl}}',
+    username: '{{lrsUsername}}',
+    password: '{{lrsPassword}}'
 };
 exports.storm = {
-  realtimeJar: '{{realtimeJar}}',
-  path: '{{stormPath}}'
+    realtimeJar: '{{realtimeJar}}',
+    path: '{{stormPath}}',
+    nimbusHost: '{{nimbusHost}}'
 };
 exports.kafka = {
-  uri: process.env.LRS_URI || process.env.LRS_URL || '{{kafkaUrl}}'
+    uri: process.env.LRS_URI || process.env.LRS_URL || '{{kafkaUrl}}'
 };
 exports.a2 = {
-  a2ApiPath: '{{a2ApiPath}}',
-  a2AdminUsername: '{{a2AdminUsername}}',
-  a2AdminPassword: '{{a2AdminPassword}}'
+    a2ApiPath: '{{a2ApiPath}}',
+    a2AdminUsername: '{{a2AdminUsername}}',
+    a2AdminPassword: '{{a2AdminPassword}}'
 };
