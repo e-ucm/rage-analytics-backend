6690284eacbc9f374f2b64116a98fc72261a1adf
RotaruDan <drotaru@ucm.es>
2016-03-31 13:56:22 +0200

    Improves /collector API doc fix

--- routes/collector.js
+++ routes/collector.js
@@ -7,11 +7,15 @@ var express = require('express'),
 var collector = require('../lib/collector');
 
 /**
- * @api {post} /collector/start/:trackingCode Returns all the Sessions.
+ * @api {post} /collector/start/:trackingCode Start a tracking session for a client
  * @apiDescription This method expects an 'Authorization' header with one of the following formats
  *  1.- <Authorization, 'a:'> or <Authorization, 'a:playerName'>. The first value will create a new anonymous player
  *  while the second will try to find the player with the given 'playerName'.
- *  2.- <Authorization, 'authToken'> This format is used when the user is already authenticated.
+ *  2.- <Authorization, 'JSON Web Token'> This format is used when the user is already authenticated.
+ *  The 'JSON Web Token is the 'token' received when logging in as an identified user in the Authorization & Authentication
+ *  (A2) service: http://e-ucm.github.io/a2/#api-Login-LoginNote that if the value of the 'Authorization'
+ *  header is the 'JSON Web Token' received when logging into the Authorization & Authentication system (A2) the
+ *  'actor' field of the response will have the player name field value set to the authenticated user.
  *
  * @apiName postCollectorStart
  * @apiGroup Collector
@@ -24,6 +28,7 @@ var collector = require('../lib/collector');
  *      HTTP/1.1 200 OK
  *      {
  *          authToken: <String>,        - Used as 'Authorization' header for '/api/collector/track' requests.
+  *                                       [/api/login, more info. at: http://e-ucm.github.io/a2/#api-Login-Login]
  *          objectId: <String>,         - Used as Object.id in the following xAPI statements.
  *          actor: Object               - https://github.com/adlnet/xAPI-Spec/blob/master/xAPI.md#actor
  *      }
@@ -38,8 +43,8 @@ router.post('/start/:trackingCode', function (req, res) {
 });
 
 /**
- * @api {post} /api/collector/track Tracks data from the request body.
- * @apiDescription Note that this method expects an 'Authorization2' header with the following format
+ * @api {post} /collector/track Tracks data from the request body.
+ * @apiDescription Note that this method expects an 'Authorization' header with the following format
  *  <Authorization, 'authToken'>.
  *  The 'authToken' can be obtained by issuing a request to '/api/collector/start/:trackingCode'.
  *
