cc5e3ca86c6499207b7fdf64e7a4471b21b1d9db
RotaruDan <drotaru@ucm.es>
2017-02-17 19:13:23 +0100

    Traces converter updated, extensions saved in 'ext' subdocument

--- test/tests/tracesConverterTest.js
+++ test/tests/tracesConverterTest.js
@@ -23,8 +23,7 @@ var should = require('should');
 describe('Traces converter tests', function () {
 
     var tracesConverter = require('../../lib/tracesConverter');
-    var getElasticSearchData = tracesConverter.toElasticSearch;
-    var getRealtimeData = tracesConverter.toRealTime;
+    var getRealtimeData = tracesConverter;
     var definitionObj = {
         extensions: {
             versionId: 'testVersionId',
@@ -149,7 +148,9 @@ describe('Traces converter tests', function () {
             gameplayId: definitionObj.extensions.gameplayId,
             event: event,
             target: target,
-            progress: progress,
+            ext: {
+                progress: progress
+            },
             success: false,
             completion: false,
             timestamp: timestamp
@@ -338,10 +339,61 @@ describe('Traces converter tests', function () {
         };
         var trace = getRealtimeData(statement);
         should(trace).eql(resultTrace);
-        trace = getElasticSearchData(statement);
 
-        delete resultTrace.gameplayId;
-        delete resultTrace.versionId;
+        done();
+    });
+
+
+    it('should correctly convert result.extensions to ext object', function (done) {
+
+        var name = '57345599db69cf4200fa41d971088';
+        var event = 'skipped';
+        var target = 'testMenu';
+        var timestamp = '2016-05-16T11:48:25Z';
+        var type = 'testType';
+        var statement = {
+            id: '19de3bf2-6b7f-4399-a71b-da5f3674c8f8',
+            actor: {
+                name: name,
+                account: {
+                    homePage: 'http://a2:3000/',
+                    name: 'Anonymous'
+                }
+            },
+            verb: {
+                id: 'http://id.tincanapi.com/verb/' + event
+            },
+            object: {
+                id: 'http://example.com/games/SuperMarioBros/Screens/' + target,
+                definition: {
+                    extensions: definitionObj.extensions,
+                    type: 'https://rage.e-ucm.es/xapi/seriousgames/activities/' + type
+                }
+            },
+            result: {
+                extensions: {
+                    ext1: '123',
+                    '/ext2': '456',
+                    'asdasd/asdasd3': 0
+                }
+            },
+            timestamp: timestamp
+        };
+        var resultTrace = {
+            name: name,
+            versionId: definitionObj.extensions.versionId,
+            gameplayId: definitionObj.extensions.gameplayId,
+            event: event,
+            target: target,
+            type: type,
+            timestamp: timestamp,
+            ext: {
+                ext1: '123',
+                ext2: '456',
+                asdasd3: 0
+            }
+        };
+        var trace = getRealtimeData(statement);
         should(trace).eql(resultTrace);
 
         done();
