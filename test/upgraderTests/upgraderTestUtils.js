8987a704d04c65649cd51c2d73e30210378b0a39
Victorma Perez Colado <victormp@ucm.es>
2017-07-10 17:14:39 +0200

    Upgrader test util linter fixes

--- test/upgraderTests/upgraderTestUtils.js
+++ test/upgraderTests/upgraderTestUtils.js
@@ -1,20 +1,20 @@
 'use strict';
 
 module.exports = {
-    compareDocuments: function compare(doc1, doc2){
+    compareDocuments: function compare(doc1, doc2) {
         var equal = true;
-        if(doc1 === undefined && doc2 === undefined || doc1 === null &&  doc2 === null){
+        if (doc1 === undefined && doc2 === undefined || doc1 === null &&  doc2 === null) {
             return true;
         }
-        if(!doc1 || !doc2){
-            console.log("ERROR COMPARING VALUES (SOME VALUE IS NOT VALID): ", doc1, " AND ", doc2);
+        if (!doc1 || !doc2) {
+            console.log('ERROR COMPARING VALUES (SOME VALUE IS NOT VALID): ', doc1, ' AND ', doc2);
             return false;
         }
 
-        if(Array.isArray(doc1) && Array.isArray(doc2)) {
+        if (Array.isArray(doc1) && Array.isArray(doc2)) {
             if (doc1.length === doc2.length) {
                 doc1.every(function (subObj) {
-                    //check array elements
+                    // Check array elements
                     var founded = false;
                     doc2.some(function (subObj2) {
                         if (compare(subObj, subObj2)) {
@@ -24,7 +24,7 @@ module.exports = {
                     });
                     equal = founded;
                     return founded;
-                })
+                });
             } else {
                 equal = false;
             }
@@ -34,8 +34,8 @@ module.exports = {
                     return;
                 }
                 var val = doc1[key];
-                if (typeof(val) !== typeof({}) && typeof(val) !== typeof([])) {
-                    if (typeof(val) === typeof('string')) {
+                if (typeof (val) !== typeof ({}) && typeof (val) !== typeof ([])) {
+                    if (typeof (val) === typeof ('string')) {
                         if (val === doc2[key]) {
                             equal = true;
                             return;
@@ -48,15 +48,15 @@ module.exports = {
                             equal = false;
                             return;
                         }
-                    } 
+                    }
 
                     if (val !== doc2[key]) {
-                        console.log("ERROR COMPARING VALUES: ", val, " (", typeof(val), ") AND ", doc2[key], " (", typeof(val), ")");
+                        console.log('ERROR COMPARING VALUES: ', val, ' (', typeof (val), ') AND ', doc2[key], ' (', typeof (val), ')');
                         equal = false;
                     }
 
-                    if (typeof(val) !== typeof(doc2[key])) {
-                        console.log("ERROR COMPARING TYPES: ", val, " AND ", doc2[key]);
+                    if (typeof (val) !== typeof (doc2[key])) {
+                        console.log('ERROR COMPARING TYPES: ', val, ' AND ', doc2[key]);
                         equal = false;
                     }
 
